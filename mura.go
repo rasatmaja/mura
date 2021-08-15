2bb932a7d15bd297a9039991ba90478c10dbf9f6
Rasio Atmaja <rasioatmaja29@gmail.com>
2021-08-15 11:18:42 +0700

    refactor(gomodule): drop viper, migrate to v2 (#39)
    See the issue for details
    - Migrating form V1 to V2 #34
    - Migrate go module to v2 #35
    - Drop viper dependency #38

--- mura.go
+++ mura.go
@@ -1,32 +1,4 @@
 package mura
 
-import (
-	"reflect"
-
-	"github.com/spf13/viper"
-)
-
-// Mura is a stuct to hold viper struct
-type Mura struct{ *viper.Viper }
-
-// New is a function to initialize mura struct
-func New() *Mura {
-	return &Mura{viper.New()}
-}
-
-// BindSysEnv is a workaround to make the unmarshal work with environment variables
-// reference: https://github.com/spf13/viper/issues/761#issuecomment-626122696
-func (m *Mura) BindSysEnv(iface interface{}) {
-	ifv := reflect.ValueOf(iface)
-	if ifv.Kind() == reflect.Ptr {
-		ifv = ifv.Elem()
-	}
-	for i := 0; i < ifv.NumField(); i++ {
-		field := ifv.Type().Field(i)
-		value, ok := field.Tag.Lookup("mapstructure")
-		if !ok {
-			continue
-		}
-		m.BindEnv(value)
-	}
-}
+// New is a function to initiate mura
+func New() {}
